1bb5d3fd1b5f0c8180bfc13e29e6c02771cfb900
dnlaxe <dnlaxe@gmail.com>
2026-06-09 13:49:09 +0900

    ui: update

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div className="p-8 border m-8 rounded-lg bg-white">
       <h1 className="text-xl underline mb-8">How to use:</h1>
-      <ul className="flex flex-col gap-8 text-xl">
+      <ul className="flex flex-col gap-4 text-lg">
         <li>1. Go to playlists page</li>
         <li>2. Create a playlist</li>
         <li>3. Add shorts to your playlist</li>
