3132207fe63cc4b12c78698e171cb3fee4abf9e2
dnlaxe <dnlaxe@gmail.com>
2026-06-18 18:52:57 +0900

    style: border, bg

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-4 py-2">
+    <nav className="flex justify-between items-center px-4 py-2 bg-white border-b">
       <Link to="/">Spaced Shorts</Link>
       <div className="flex gap-4">
         <Link to="/playlists">Playlists</Link>
