8fabad3db5f2b8b00e6d230756ed7c0a4ae13c32
dnlaxe <dnlaxe@gmail.com>
2026-06-18 18:57:39 +0900

    style: instructions

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,15 +1,25 @@
 export default function HomePage() {
   return (
     <div className="p-8 border m-8 rounded-lg bg-white">
-      <h1 className="text-xl underline mb-8">How to use:</h1>
       <ul className="flex flex-col gap-4 text-lg">
-        <li>1. Go to playlists page</li>
-        <li>2. Create a playlist</li>
-        <li>3. Add shorts to your playlist</li>
-        <li>4. Click practice to see shorts and then decide its difficulty</li>
         <li>
-          5. Spaced repetition algorithm will then decide when to show that
-          short to you again
+          <span className="font-black text-xl mr-2">1</span> Go to playlists
+          page
+        </li>
+        <li>
+          <span className="font-black text-xl mr-2">2</span> Create a playlist
+        </li>
+        <li>
+          <span className="font-black text-xl mr-2">3</span> Add shorts to your
+          playlist
+        </li>
+        <li>
+          <span className="font-black text-xl mr-2">4</span> Click practice to
+          see shorts and then decide its difficulty
+        </li>
+        <li>
+          <span className="font-black text-xl mr-2">5</span> Spaced repetition
+          algorithm will then decide when to show that short to you again
         </li>
       </ul>
     </div>
